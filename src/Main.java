c83417077a9031395351023bfb047bee90b61964
DenTyutyunik <den.tyutyunik@gmail.com>
2024-07-07 19:26:03 +0400

    homework was revised

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // Task 1
         System.out.println("Task 1");
-        double [] salaryPerWorkers = {80_000, 120_000, 66_000, 170_000, 100_000};
+        double[] salaryPerWorkers = {80_000, 120_000, 66_000, 170_000, 100_000};
         double salaryAllWorkers = 0;
         for (int i = 0; i < salaryPerWorkers.length; i++) {
             salaryAllWorkers += salaryPerWorkers[i];
@@ -12,7 +12,7 @@ public class Main {
         // Task 2
         System.out.println();
         System.out.println("Task 2");
-        double [] spendMoneyForWeek = {80, 120, 66, 170, 100};
+        double[] spendMoneyForWeek = {80, 120, 66, 170, 100};
         double spendMoneyMin = spendMoneyForWeek[0];
         double spendMoneyMax = spendMoneyForWeek[0];
         for (int i = 0; i < spendMoneyForWeek.length; i++) {
@@ -28,7 +28,7 @@ public class Main {
         // Task 3
         System.out.println();
         System.out.println("Task 3");
-        int [] spendMoneyEveryWeek = {13_000, 20_000, 30_000, 12_000, 17_000};
+        int[] spendMoneyEveryWeek = {13_000, 20_000, 30_000, 12_000, 17_000};
         double spendMoneyForMonth = 0;
         double spendMoneyAverage = 0;
         for (int i = 0; i < spendMoneyEveryWeek.length; i++) {
@@ -40,13 +40,8 @@ public class Main {
         // Task 4
         System.out.println();
         System.out.println("Task 4");
-        String [] fullName = {"n", "a", "v", "I", " ", "v", "o", "n", "a", "v", "I"};
-        for (int i = 0, j = fullName.length - 1; i < j; i++, j--) {
-            String temp = fullName[i];
-            fullName[i] = fullName[j];
-            fullName[j] = temp;
-        }
-        for (int i = 0; i < fullName.length; i++) {
+        String[] fullName = {"n", "a", "v", "I", " ", "v", "o", "n", "a", "v", "I"};
+        for (int i = fullName.length - 1; i >= 0; i--) {
             System.out.print(fullName[i]);
         }
     }
